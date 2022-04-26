ce781e5ba94c95689e2931a9c1e9340248f3964c
Eiden <eidnegabriel07@gmail.com>
2022-04-26 10:25:11 -0400

    task 1,2 adn 3

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,11 @@ number = random.randint(-10000, 10000)
 lastd = int(repr(number)[-1])
 
 if lastd > 5:
-    print("Last digit of {0} is {1} and is greater than 5".format(number, lastd))
+    print("Last digit of {0} is {1} and is greater than 5"
+          .format(number, lastd))
 if lastd == 0:
-    print("Last digit of {0} is {1} and is 0".format(number, lastd))
+    print("Last digit of {0} is {1} and is 0"
+          .format(number, lastd))
 if lastd < 6 and lastd != 0:
-    print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, lastd))
-    
+    print("Last digit of {0} is {1} and is less than 6 and not 0"
+          .format(number, lastd))
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
 for i in range(97, 123):
-    sys.stdout.write(chr(i))
+    print(chr(i), end="")
