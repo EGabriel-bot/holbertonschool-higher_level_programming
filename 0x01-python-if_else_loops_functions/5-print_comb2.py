a32631c855700bfb03a3f7100d7714e1922aefb8
Eiden <eidnegabriel07@gmail.com>
2022-04-26 13:21:42 -0400

    fix task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 for i in range(100):
     num = i % 10
     num2 = i // 10
-    
-    print("{:d}{:d} ".format(num2, num), end="")
+
+    print("{:d}{:d}".format(num2, num), end="")
     if i != 99:
-      print(",", end= "")
+      print(" ,", end= "")
