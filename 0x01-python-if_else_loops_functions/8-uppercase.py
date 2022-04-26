a077a0b4f6502c7fe30e8acadb5dc4b28824ce80
Eiden <eidnegabriel07@gmail.com>
2022-04-26 16:41:10 -0400

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper = " "
+    for i in (str):
+        char = ord(i)
+        if char >= ord('a') and char <= ord('z'):
+            char = char - 32
+            upper = chr(char)
+        else:
+            upper = i
+            print("{:s}".format(upper), end="")
+    print("")
