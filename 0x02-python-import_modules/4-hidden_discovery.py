67e02a09a9c710be92b077a6d5e7103756e2eddb
Eiden <eidnegabriel07@gmail.com>
2022-04-28 15:01:20 -0400

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import hidden_4
-
-idx = 0
-module = dir(hidden_4)
-for i in range (len(module)):
-  word = module[idx]
-  if word[0] != "_":
-    print(word)
-  idx += 1
+if __name__ == "__main__":
+    idx = 0
+    module = dir(hidden_4)
+    for i in range(len(module)):
+        word = module[idx]
+        if word[0] != "_":
+            print(word)
+        idx += 1
