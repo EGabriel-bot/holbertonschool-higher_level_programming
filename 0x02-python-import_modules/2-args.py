4d679860e9a153e1c013c0056b3f6488b99674ee
Eiden <eidnegabriel07@gmail.com>
2022-04-28 13:07:11 -0400

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+
+    i = 1
+
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif argv == 2:
+        print("{} arguments:".format(len(argv) - 1))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+    while i + 1 < len(argv) + 1:
+        print(f"{i}:{argv[i]}")
+        i = i + 1
