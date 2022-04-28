f9c78a17fff8cff10c62dd866f305e13b25445a8
Eiden <eidnegabriel07@gmail.com>
2022-04-28 12:48:39 -0400

    fix task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 if __name__ == "__main__":
 
     a = 1
     b = 2
-    c = add_0.add(a, b)
+    c = add(a, b)
 
     print(f"{a} + {b} = {c}")
