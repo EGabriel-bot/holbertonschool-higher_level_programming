0c9850e31c4805c1da01fcecfa7136ca58733095
Eiden <eidnegabriel07@gmail.com>
2022-05-02 19:39:33 -0400

    fix task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.sort(reverse=True)
-    idx = 0
-    for i in my_list:
-        print(my_list[idx])
-        idx = idx + 1
+    for i in range(len(my_list)):
+        print(my_list[i])
+        i = i + 1
