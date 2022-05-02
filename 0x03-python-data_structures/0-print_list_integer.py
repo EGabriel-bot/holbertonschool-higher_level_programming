74857b1f72a9a2f4e5b91989c0ad17409be1c890
Eiden <eidnegabriel07@gmail.com>
2022-05-02 12:14:01 -0400

    task 0,1,2,3 and readme

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+  #for i in my_list:
+    #print("{:d}".format(my_list.index(i) + 1))
+  print(*my_list, sep= "\n")
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,4 @@
+def element_at(my_list, idx):
+    for i in my_list:
+        if(idx == my_list[idx] - 1):
+            return my_list[idx]
