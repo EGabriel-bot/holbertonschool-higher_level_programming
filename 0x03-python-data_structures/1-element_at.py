99b81af88225fcd1c0c300af824d8cc9c08ae7e7
Eiden <eidnegabriel07@gmail.com>
2022-05-02 13:25:05 -0400

    fix task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,3 +3,7 @@ def element_at(my_list, idx):
     for i in my_list:
         if(idx == my_list[idx] - 1):
             return my_list[idx]
+        if idx < 0:
+            return None
+        if idx > len(my_list):
+            return None
