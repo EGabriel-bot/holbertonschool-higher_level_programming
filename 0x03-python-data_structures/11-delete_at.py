59e4dd0a2a8cf256ed399a76e738642c4d91eea0
Eiden <eidnegabriel07@gmail.com>
2022-05-03 08:01:42 -0400

    fix task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,5 +4,5 @@ def delete_at(my_list=[], idx=0):
         if idx < 0 or idx >= len(my_list):
             return my_list
         if i == my_list[idx]:
-            del my_list[i]
+            my_list.remove(i)
             return my_list
