9d0eb811e503be82d72df43c4b30ed4b650c7ff9
Eiden <eidnegabriel07@gmail.com>
2022-05-04 12:31:18 -0400

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = list(map(list, matrix))
+    for i in range(len(new)):
+        for j in range(len(new)):
+            new[i][j] = new[i][j] * new[i][j]
+
+    return new
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,2 @@
+project 0x04-
+More Data structures: Set, dictionary
