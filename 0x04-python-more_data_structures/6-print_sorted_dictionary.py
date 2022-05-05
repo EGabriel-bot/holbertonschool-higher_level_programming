308113ed146dd4dbad49403256ff5b95768e9e99
Eiden <eidnegabriel07@gmail.com>
2022-05-04 20:18:20 -0400

    fix task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print(f"Number: {a_dictionary.get('number')}")
+    print(f"Number: {a_dictionary.get('Number')}")
     print(f"ids: {a_dictionary.get('ids')}")
     print(f"language: {a_dictionary.get('language')}")
     print(f"track: {a_dictionary.get('track')}")
