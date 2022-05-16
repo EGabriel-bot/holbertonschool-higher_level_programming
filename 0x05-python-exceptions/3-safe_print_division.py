5e58809bbbd211c98d2b13a14dd874ba9c5f69f2
Eiden <eidnegabriel07@gmail.com>
2022-05-16 13:45:13 -0400

    task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        div = a/b
+        return div
+    except ZeroDivisionError:
+        div = "None"
+        return None
+    finally:
+        print("Inside result: ", end="")
+        print(div)
