6ed027c5a6b1eec8bb2931cdb4fe576404d121e6
Eiden <eidnegabriel07@gmail.com>
2022-05-23 20:01:53 -0400

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""File comment"""
+
+
+class Rectangle:
+    """comment"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+        @property
+        def width(self):
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            if not type(value) is int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+
+        @property
+        def height(self):
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            if not type(value) is [int]:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
