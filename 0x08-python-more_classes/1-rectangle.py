d6da005d5aca3829f4d3430074eacd23c9c35850
Eiden <eidnegabriel07@gmail.com>
2022-05-23 20:40:22 -0400

    fix task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     __height = None
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
