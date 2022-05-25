34214aa17a89ae639e71ac430613e13c7d85f0b9
Eiden <eidnegabriel07@gmail.com>
2022-05-25 13:45:50 -0400

    task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""File comment"""
+
+
+class Rectangle:
+    """comment"""
+
+    __width = None
+    __height = None
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            self.perimeter = 0
+            return self.perimeter
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        sep = ""
+        list = []
+        if self.width == 0 or self.height == 0:
+            return("")
+        for i in range(self.height):
+            list.append("#" * self.width)
+            if i != self.height - 1:
+                list.append("\n")
+        return sep.join(list)
