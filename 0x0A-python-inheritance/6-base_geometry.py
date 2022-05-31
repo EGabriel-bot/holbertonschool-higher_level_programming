6f7f5ac6316c1520ee964ec6e0c91ed86ca06f39
Eiden <eidnegabriel07@gmail.com>
2022-05-31 09:30:38 -0400

    fix task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Improve Geometry"""
+
+
 class BaseGeometry:
+    """Public instance method"""
+
     def area(self):
+        """raises an Exception"""
         raise Exception("area() is not implemented")
