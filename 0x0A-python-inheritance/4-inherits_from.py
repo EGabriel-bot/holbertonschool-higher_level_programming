b89dd0ef8fcb54e4039f189e8cad45aba63fea6c
Eiden <eidnegabriel07@gmail.com>
2022-05-31 09:16:40 -0400

    fix task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Only sub class of"""
+
+
 def inherits_from(obj, a_class):
+    """returns True if the object
+    is an instance of a class that inherited (directly or indirectly)
+    from the specified class; otherwise False"""
+
     if not isinstance(obj, a_class) or type(obj) is a_class:
         return False
     else:
