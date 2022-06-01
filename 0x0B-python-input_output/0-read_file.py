6ec06f6f92d1dfb027ef0d6ce59076a6244b4b2d
Eiden <eidnegabriel07@gmail.com>
2022-06-01 17:05:47 -0400

    fix task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """reads a text file and prints it to stdout"""
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(read_data)
