9bd623c241e01b095924884bc26ac7923c669ea7
Eiden <eidnegabriel07@gmail.com>
2022-06-08 12:46:04 -0400

    fix task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,7 +36,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """from json string"""
-        if not isinstance(json_string, str) or len(json_string) == 0:
+        if not isinstance(json_string, str) or json_string == "":
             return []
         else:
             return json.loads(json_string)
