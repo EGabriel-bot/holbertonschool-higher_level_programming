0abd9253186071820eb749319e5cdbd0730586df
Eiden Martinez Davila <eidengabriel07@gmail.com>
2022-06-07 22:07:56 -0400

    fix task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,10 +15,12 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ to json method """
         return json.dumps(list_dictionaries)
 
+    @classmethod
     def save_to_file(cls, list_onjs):
         """ save to file method """
 
@@ -32,7 +34,6 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ from json string method """
-        empty = []
         if not isinstance(json_string, str) or len(json_string) == 0:
             return []
         else:
