9820591d4ab0b30ff5b471f8bc495d174f735146
Eiden Martinez Davila <eidengabriel07@gmail.com>
2022-06-07 22:24:41 -0400

    fix task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ to json method """
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries is False:
             return []
         else:
             return json.dumps(list_dictionaries)
