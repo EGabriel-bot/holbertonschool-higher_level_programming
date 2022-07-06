aeeb06fd015f5bf46eaf75901689e6921a888953
Eiden <eidnegabriel07@gmail.com>
2022-07-06 12:50:10 -0400

    fix task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,12 @@
+-- creates a table second_table in the database hbtn_0c_0
+-- second_table description: id INT, name VARCHAR(256), score INT
 CREATE TABLE IF NOT EXISTS second_table(
   id INT,
   name VARCHAR(256),
   score INT
 );
 
+-- Insert values to second table
 INSERT INTO second_table (
   id,
   name,
@@ -21,4 +24,4 @@ VALUES
 ),
 (
   4,"George",8
-)
+);
