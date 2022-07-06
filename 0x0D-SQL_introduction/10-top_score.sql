09a505910b99ddae77a0dfbb53bd1f16614eaa9c
Eiden <eidnegabriel07@gmail.com>
2022-07-06 13:11:05 -0400

    task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+--  lists all records of the table second_table of the database hbtn_0c_0
+--  Display score and name records. Records should be ordered by score (top first) 
+SELECT score,name FROM second_table ORDER BY score DESC;
