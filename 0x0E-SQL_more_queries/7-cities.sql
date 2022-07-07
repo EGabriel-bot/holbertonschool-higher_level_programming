45e77aecda2daba3fd0572a9d308fd1a3cd6c235
Eiden <eidnegabriel07@gmail.com>
2022-07-07 13:48:05 -0400

    task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Cities table
+-- script that creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Creates table cities in the hbtn_0d_usa database
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+  id INT AUTO_INCREMENT,
+  state_id INT NOT NULL,
+  PRIMARY KEY (id) 
+  FOREIGN KEY (state_id) REFERENCES (states)
+);
