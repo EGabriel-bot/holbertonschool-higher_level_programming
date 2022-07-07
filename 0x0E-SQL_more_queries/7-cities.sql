f2bdabc52873349e56310b6f2cacd5516185386c
Eiden <eidnegabriel07@gmail.com>
2022-07-07 15:08:11 -0400

    ?

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
   state_id INT,
   name VARCHAR(256) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (state_id) REFERENCES states (id)
+  FOREIGN KEY (state_id) REFERENCES states(id)
 );
