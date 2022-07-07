1e43bdb9aa4faa9e6b63d367ace1ec3a75068820
Eiden <eidnegabriel07@gmail.com>
2022-07-07 13:13:33 -0400

    fix task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- States table
--- creates the database hbtn_0d_usa and the table states in the database hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+-- creates the database hbtn_0d_usa in the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- create a table named states
 CREATE TABLE IF NOT EXISTS states(
   id INT AUTO_INCREMENT,
   name VARCHAR(256) NOT NULL,
