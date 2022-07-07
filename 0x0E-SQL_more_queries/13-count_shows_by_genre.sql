32eec4ef83f557fbf8b5e6a55a28a4f3eaa63bea
Eiden <eidnegabriel07@gmail.com>
2022-07-07 19:29:03 -0400

    fix task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- No genre
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT g.genres, s.number_of_shows
+SELECT g.genre, s.number_of_shows
 FROM tv_genres AS g
 LEFT JOIN tv_show_genres AS s
 ON g.name = s.show_id
-WHERE s.show_id IS NULL
+WHERE s.show_id IS NOT NULL
 ORDER BY s.number_of_shows DESC;
