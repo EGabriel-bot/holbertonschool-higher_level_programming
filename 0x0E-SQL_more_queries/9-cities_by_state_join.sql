95cecd61434572ae54fcc445a868518ca03f1b74
Eiden <eidnegabriel07@gmail.com>
2022-07-07 16:17:38 -0400

    task 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Cities by States
+-- lists all cities contained in the database hbtn_0d_usa
+SELECT c.id, c.name, s.name 
+FROM cities as c 
+INNER JOIN states as s
+ON c.state_id = s.state_id
+ORDER BY c.id ASC;
