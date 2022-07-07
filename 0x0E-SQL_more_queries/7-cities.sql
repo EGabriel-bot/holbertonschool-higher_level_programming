19562af5b4f4f7ead7db5fb05714cf092bc908c8
Eiden <eidnegabriel07@gmail.com>
2022-07-07 15:05:52 -0400

    quick fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Creates table cities in the hbtn_0d_usa database
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
   id INT AUTO_INCREMENT,
-  state_id INT NOT NULL,
+  state_id INT,
   name VARCHAR(256) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (state_id) REFERENCES states (id)
