b0399298468fff24ff8cd25e1ef5647a5c03bc85
Eiden <eidnegabriel07@gmail.com>
2022-07-07 14:23:12 -0400

    name was missing task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
   id INT AUTO_INCREMENT,
   state_id INT NOT NULL,
+  name VARCHAR(256) NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT FK_states FOREIGN KEY (state_id) 
-  REFERENCES states (id)
+  FOREIGN KEY (state_id) REFERENCES states (id)
 );
