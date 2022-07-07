75f82ae74aab3da12f8628c1d8436d460ceda373
Eiden <eidnegabriel07@gmail.com>
2022-07-07 13:30:11 -0400

    fix task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,4 +6,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grants the user user_0d_2 only the select privilege
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
