c705b202c0a2e2fe055df9f002be6a74f27f542f
Eiden <eidnegabriel07@gmail.com>
2022-07-07 16:23:17 -0400

    fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- Cities by States
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT c.id, c.name, s.name 
-FROM cities as c 
-INNER JOIN states as s
+FROM cities AS c
+INNER JOIN states AS s
 ON c.state_id = s.id
 ORDER BY c.id ASC;
