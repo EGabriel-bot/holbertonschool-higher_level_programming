b30a2ffdd8cd8a5df0a189b554cc7449ea324fa1
Eiden <eidnegabriel07@gmail.com>
2022-08-01 12:55:05 -0400

    fix task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ function add (a, b) {
   return (a + b);
 }
 const argument = process.argv.length;
-if (argument[2] === NaN || argument === 2) {
+if (isNaN(argument[2]) || argument === 2) {
   console.log(NaN);
 } else {
   console.log(add(Number(process.argv[2]), Number(process.argv[3])));
