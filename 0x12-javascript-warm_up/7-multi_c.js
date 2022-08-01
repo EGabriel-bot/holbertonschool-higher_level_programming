0f2d45df163fe6442e87fc6a50b206fdd0589a6a
Eiden <eidnegabriel07@gmail.com>
2022-08-01 12:51:02 -0400

    fix task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const times = process.argv[2];
-if (process.argv[2] === undefined) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 }
 for (let i = 0; i < times; i++) {
