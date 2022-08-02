60fcfa29bbe6af3a1e53d3d1812763ec26ad0a23
Eiden <eidnegabriel07@gmail.com>
2022-08-02 08:08:44 -0400

    fix task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const argument = process.argv.length;
-if (argument === 2) {
+if (process.argv[2] === undefined ) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
