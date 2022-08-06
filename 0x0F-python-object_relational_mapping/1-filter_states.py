d74f9f269e04da4157bd146c50cbee1fef91bc12
Eiden <eidnegabriel07@gmail.com>
2022-08-06 13:39:30 -0400

    fix task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
         host="localhost", port=3306, user=sys.argv[1],
         password=sys.argv[2], database=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    c.execute("SELECT * FROM states WHERE name LIKE 'N_%' ORDER BY id ASC;")
     [print(state) for state in c.fetchall()]
