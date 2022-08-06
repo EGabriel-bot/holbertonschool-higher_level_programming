c1d55c3560fcc62b8ee48a108f18fea7e5f0915b
Eiden <eidnegabriel07@gmail.com>
2022-08-06 13:13:51 -0400

    task 0 - 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""lists all states with a name starting with N"""
 if __name__ == '__main__':
     import sys
     import MySQLdb
@@ -8,5 +8,5 @@ if __name__ == '__main__':
         host="localhost", port=3306, user=sys.argv[1],
         password=sys.argv[2], database=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states;")
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
     [print(state) for state in c.fetchall()]
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""displays all values in the states table of hbtn_0e_0_usa
+ where name matches the argument"""
 if __name__ == '__main__':
     import sys
     import MySQLdb
@@ -7,6 +8,10 @@ if __name__ == '__main__':
     db = MySQLdb.connect(
         host="localhost", port=3306, user=sys.argv[1],
         password=sys.argv[2], database=sys.argv[3])
+    name = sys.argv[4]
     c = db.cursor()
-    c.execute("SELECT * FROM states;")
+    query = "SELECT * FROM states WHERE name LIKE '{name}%'\
+         ORDER BY id ASC".format(
+        name=sys.argv[4])
+    c.execute(query)
     [print(state) for state in c.fetchall()]
