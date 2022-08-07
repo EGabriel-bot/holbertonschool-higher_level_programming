041efe01abe8cf277cfbbbad5ee6f5432b7df1fb
Eiden <eidnegabriel07@gmail.com>
2022-08-07 16:11:56 -0400

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start link to table in database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """create engine to execute queries"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter(State.name.like('%a%'))
+
+    for row in result:
+        print("{}: {}".format(row.id, row.name))
