ffa3a78c94f3f9ffe5fa37294964753bb977f86e
Eiden <eidnegabriel07@gmail.com>
2022-08-07 18:33:25 -0400

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Start link to table in database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """create engine to execute queries"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).get(2)
+    result.name = 'New Mexico'
+    session.commit()
