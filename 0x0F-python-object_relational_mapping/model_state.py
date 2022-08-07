ab8bbbc6342de17037fb155ba512f353747d93f2
Eiden <eidnegabriel07@gmail.com>
2022-08-07 13:35:01 -0400

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin python3
+"""declaring columns for a table"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class state that inherits from base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(128), nullable=False)
