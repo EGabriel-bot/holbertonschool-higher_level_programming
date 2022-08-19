e1babfe3ce0d6448015e018b14b831d9d17816dd
Eiden <eidnegabriel07@gmail.com>
2022-08-19 17:31:10 -0400

    fix task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """fetches url passed as an argument"""
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-r = requests.get(sys.argv[1])
-print(r.headers['X-Request-Id'])
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
