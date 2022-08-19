2175d780eb256c9a20f71110ec358309aa1fe29e
Eiden <eidnegabriel07@gmail.com>
2022-08-19 16:03:55 -0400

    fix task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from urllib import request
     import sys
 
-    with request.urlopen(f'{sys.argv[1]}') as response:
+    with request.urlopen('{}'.format(sys.argv[1])) as response:
         header = response.info()
 
     print(header['X-Request-Id'])
