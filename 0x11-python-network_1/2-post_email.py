f044f6bcc2e6679b37e3d0b0e7cab34997591150
Eiden <eidnegabriel07@gmail.com>
2022-08-19 16:28:40 -0400

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.parse
+
+    url = sys.argv[1]
+    params = {
+        "email": sys.argv[2],
+    }
+
+    query_string = urllib.parse.urlencode(params)
+    data = query_string.decode("ascii")
+
+    with urllib.request.urlopen(url, data) as response:
+        response_text = response.read()
+        print(response_text.decode('utf-8'))
