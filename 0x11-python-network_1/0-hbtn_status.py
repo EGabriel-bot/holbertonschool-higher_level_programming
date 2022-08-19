c060d628b42e9d84d1f8cddb77c19e1b0c2d9635
Eiden <eidnegabriel07@gmail.com>
2022-08-19 15:32:56 -0400

    fix task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
 if __name__ == "__main__":
     import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
