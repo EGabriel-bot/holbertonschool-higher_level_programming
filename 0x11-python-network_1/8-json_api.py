892912af5ad519758e1774e49291298308cd7a32
Eiden <eidnegabriel07@gmail.com>
2022-08-20 10:38:45 -0400

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""POST request to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    info = {"q": letter}
+
+    req = requests.post(url, data=info)
+    try:
+        response = req.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
