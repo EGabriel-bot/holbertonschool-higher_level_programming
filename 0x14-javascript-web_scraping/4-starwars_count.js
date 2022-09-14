f56e4019b29a880f50e148ece9c9547f3ca38edc
Eiden <eidnegabriel07@gmail.com>
2022-09-14 10:38:27 -0400

    fix task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,15 +4,15 @@ let appearance = 0;
 axios.get(process.argv[2])
   .then(res => {
     for (let blockIndex = 0; blockIndex < res.data.results.length; blockIndex++) {
-      if (res.data.results[blockIndex]){
-        for (let characterIndex = 0; characterIndex < res.data.results[blockIndex].characters.length; characterIndex++){
+      if (res.data.results[blockIndex]) {
+        for (let characterIndex = 0; characterIndex < res.data.results[blockIndex].characters.length; characterIndex++) {
           if (res.data.results[blockIndex].characters[characterIndex].includes('18')) {
-              appearance++;
-        };
-        };
-      };
-    };
-    console.log(appearance)
+            appearance++;
+          }
+        }
+      }
+    }
+    console.log(appearance);
   })
   .catch(error => {
     if (error.response) {
