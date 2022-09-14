c25c8f219f33d71cd2c80a72c67ca67758c614e4
Eiden <eidnegabriel07@gmail.com>
2022-09-14 10:11:19 -0400

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 const axios = require('axios');
 let appearance = 0;
+const character = 'https://swapi-api.hbtn.io/api/people/18/';
 axios.get(process.argv[2])
   .then(res => {
-    for (let i = 0; i < res.data.count; i++) {
-      console.log('enter_loop');
-      if (res.data.characters === 'https://swapi-api.hbtn.io/api/people/18/') {
-        console.log('enter_if');
-        appearance++;
-        console.log('increment');
-      } else { continue; }
+    for (let blockIndex = 0; blockIndex < res.data.results.length; blockIndex++) {
+      if (res.data.results[blockIndex]) {
+        for (let characterIndex = 0; characterIndex < res.data.results[blockIndex].characters.length; characterIndex++) {
+          if (res.data.results[blockIndex].characters[characterIndex] === character) {
+            appearance++;
+          }
+        }
+      }
     }
-    console.log('not increment');
     console.log(appearance);
   })
   .catch(error => {
