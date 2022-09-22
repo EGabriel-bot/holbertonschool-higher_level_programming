7645071874158a98593c465361cbc481bc758c05
Eiden <eidnegabriel07@gmail.com>
2022-09-22 09:09:21 -0400

    task 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,3 @@
-$.get('https://stefanbohacek.com/hellosalut/?lang=fr', function (data){
+$.get('https://stefanbohacek.com/hellosalut/?lang=fr', function (data) {
   $('#hello').text(`${data.hello}`);
-})
+});
